e3d53b5295e88a09a165617984fa3e5ce8663e37
srivathsabevur <srivathsabevur@gmail.com>
2024-06-28 20:07:55 +0530

    Changed color of dashboard

--- src/components/dashboard/sidebar.jsx
+++ src/components/dashboard/sidebar.jsx
@@ -35,7 +35,7 @@ export default function Sidebar()
       };
     
     const [showSideBar, setshowSideBar] = useState(true)
-    const [showCard, setShowCard] = useState("")
+    const [showCard, setShowCard] = useState("Dashboard")
     
 
     return(
@@ -44,18 +44,18 @@ export default function Sidebar()
                 initial={{ x: "-100%" }}
                 animate={{ x: showSideBar ? "0" : "-100%" }}
                 transition={{ duration: 0.5 }}
-                className={`${showSideBar ? "" : "hidden"} bg-gradient-to-b from-blue-500 to-teal-200 text-white md:fixed z-10 lg:flex gap-1 flex-col pl-5 pt-4 fixed left-0 top-0 lg:w-[18%] h-screen job-details-container`}
+                className={`${showSideBar ? "" : "hidden"} bg-gradient-to-b from-[#004AAD] via-[#004AAD] via-50% to-[#62B8BD] text-white md:fixed z-10 lg:flex gap-1 flex-col pl-5 pt-4 fixed left-0 top-0 lg:w-[18%] h-screen job-details-container`}
             >
-                <div className="flex justify-around">
+                <div className="flex justify-around mb-10">
                     <div className="flex flex-col justify-center items-start text-left my-3">
                         <h1 className="bg-gradient-to-b from-blue-500 to-teal-200 inline-block text-transparent bg-clip-text md:text-3xl">Yuvamytr</h1>
-                        <h2 className=" text-black  text-sm flex md:text-2xl">ACE Dashboard</h2>
+                        <h2 className=" text-white  text-sm flex md:text-2xl">ACE Tutor Dashboard</h2>
                     </div>
                     <button onClick={()=>setShowSideBar(!showSideBar)} className="z-50 mb-4 relative px-2 lg:hidden font-bold">{showSideBar?<FaAngleDoubleLeft className="lg:hidden ml-auto text-2xl"/>:<FaAngleDoubleRight className="lg:hidden z-50 ml-auto text-2xl"/>}</button>
                 </div>
-                <button onClick={()=>setShowCard("Analytics")} className={`${showCard=='Analytics'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal`}><SiGoogleanalytics className="mr-4 text-xl"/> Analytics</button>
-                <button onClick={()=>setShowCard("Assessments")} className={`${showCard=='Assessments'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md  hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal`}><MdOutlineAssessment className="mr-4 text-xl"/>Assessments</button>
-                <button onClick={()=>setShowCard("Assignments")} className={`${showCard=='Assignments'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal`}><MdAssignment className="mr-4 text-xl"/> Assignments</button>
+                <button onClick={()=>setShowCard("Dashboard")} className={`${showCard=='Dashboard'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal text-xl`}><SiGoogleanalytics className="mr-4 text-xl"/> Dashboard</button>
+                <button onClick={()=>setShowCard("Assessments")} className={`${showCard=='Assessments'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md  hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal text-xl`}><MdOutlineAssessment className="mr-4 text-xl"/>Assessments</button>
+                <button onClick={()=>setShowCard("Assignments")} className={`${showCard=='Assignments'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal text-xl`}><MdAssignment className="mr-4 text-xl"/> Assignments</button>
                 {/* <button onClick={()=>setShowCard("Attendence")} className={`${showCard=='Attendence'?"font-semibold bg-white text-black":""} px-4 py-4 w-full text-left rounded-tl-md rounded-bl-md hover:shadow-sm hover:shadow-gray-600 flex items-center font-normal`}><HiMiniUserGroup className="mr-4 text-xl"/> Attendence</button>  */}
                 <button  className={`absolute float bottom-20 px-4 py-4 w-full text-left flex items-center justify-start font-normal hover:text-lg `} onClick={handleLogout}><IoIosLogOut className="mr-4 text-xl"/> Logout</button>             
             </motion.div>
